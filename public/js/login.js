720f14464541c3817c2d284112fe37e7a9dc0246
Omar <62365988+omata48@users.noreply.github.com>
2020-07-30 10:35:38 -0500

    linter cleanup

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   // Getting references to our form and inputs
-  var loginForm = $("form.login");
-  var usernameInput = $("input#username-input");
-  var passwordInput = $("input#password-input");
+  var loginForm = $('form.login');
+  var usernameInput = $('input#username-input');
+  var passwordInput = $('input#password-input');
 
   // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function(event) {
+  loginForm.on('submit', function(event) {
     event.preventDefault();
     console.log(usernameInput, passwordInput);
     var userData = {
@@ -19,18 +19,18 @@ $(document).ready(function() {
 
     // If we have an email and password we run the loginUser function and clear the form
     loginUser(userData.username, userData.password);
-    usernameInput.val("");
-    passwordInput.val("");
+    usernameInput.val('');
+    passwordInput.val('');
   });
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
   function loginUser(username, password) {
-    $.post("/api/login", {
+    $.post('/api/login', {
       username: username,
       password: password
     })
       .then(function() {
-        window.location.replace("/members");
+        window.location.replace('/members');
         // If there's an error, log the error
       })
       .catch(function(err) {
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   // Getting references to our form and input
-  var signUpForm = $("form.signup");
-  var usernameInput = $("input#username-input");
-  var nameInput = $("input#name-input");
-  var passwordInput = $("input#password-input");
-  var DOBInput = $("input#DOB-input");
-  var signInput = $("input#sign-input");
+  var signUpForm = $('form.signup');
+  var usernameInput = $('input#username-input');
+  var nameInput = $('input#name-input');
+  var passwordInput = $('input#password-input');
+  var DOBInput = $('input#DOB-input');
+  var signInput = $('input#sign-input');
 
   // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", function(event) {
+  signUpForm.on('submit', function(event) {
     event.preventDefault();
     console.log(signInput.val());
     var userData = {
@@ -36,18 +36,18 @@ $(document).ready(function() {
       userData.DOB,
       userData.sign
     );
-    usernameInput.val("");
-    nameInput.val("");
-    passwordInput.val("");
-    DOBInput.val("");
-    signInput.val("");
+    usernameInput.val('');
+    nameInput.val('');
+    passwordInput.val('');
+    DOBInput.val('');
+    signInput.val('');
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
 
   function signUpUser(username, password, name, DOB, sign) {
-    $.post("/api/signup", {
+    $.post('/api/signup', {
       username: username,
       password: password,
       name: name,
@@ -55,7 +55,7 @@ $(document).ready(function() {
       sign: sign,
     })
       .then(function() {
-        window.location.replace("/members");
+        window.location.replace('/members');
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
@@ -63,7 +63,7 @@ $(document).ready(function() {
 
   function handleLoginErr(err) {
     console.log(err.responseJSON);
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
+    $('#alert .msg').text(err.responseJSON);
+    $('#alert').fadeIn(500);
   }
 });
