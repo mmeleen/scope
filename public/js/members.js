23d2d454e46a83f15d3ed5984e840ab6377d1602
Matt Meleen <mmeleen@utexas.edu>
2020-07-31 11:34:58 -0500

    API routes for searches

--- public/js/members.js
+++ public/js/members.js
@@ -1,18 +1,33 @@
 $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.name);
+  $.get('/api/user_data').then(function(data) {
+    $('.member-name').text(data.name);
   });
 
-  $.get("/api/allHoroscopes/", function(results) {
+  $.get('/api/allHoroscopes/', function(results) {
     console.log(results);
     console.log(results.yesterday.description);
     console.log(results.today.description);
     console.log(results.tomorrow.description);
 
-    $("#yesterdays-description").text(results.yesterday.description);
-    $("#todays-description").text(results.today.description);
-    $("#tomorrows-description").text(results.tomorrow.description);
+    $('#yesterdays-description').text(results.yesterday.description);
+    $('#todays-description').text(results.today.description);
+    $('#tomorrows-description').text(results.tomorrow.description);
+  });
+
+  $('#save').on('click', function(event) {
+    event.preventDefault();
+    var savedScope = {
+      date: $('#').val(),
+      description: $('#').val(),
+      mood: $('#').val(),
+      color: $('#').val(),
+      lucky_number: $('#').val(),
+      lucky_time: $('#').val()
+    };
+    $.post('/api/saveSearch/', savedScope, function(data) {
+      console.log(data);
+    });
   });
 });
