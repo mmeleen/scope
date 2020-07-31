24bd1a31c7c8aa56a7c32a566ff741a657518bb2
Osyrus85 <osyrus85@gmail.com>
2020-07-31 14:00:30 -0500

    Updated html routes compatability js style for card on compatability

--- public/js/compatibility.js
+++ public/js/compatibility.js
@@ -1,54 +1,50 @@
 // var axios = require('axios');
 
-
-
 $(document).ready(function () {
-   function compatibility(yourName, theirName, yourDOB, theirDOB,) {
+  function compatibility(yourName, theirName, yourDOB, theirDOB) {
     var settings = {
-        "async": true,
-        "crossDomain": true,
-        method:"POST",
-        url:"https://astrology-horoscope.p.rapidapi.com/zodiac_compatibility/result",
-        headers:{
-        "content-type":"application/x-www-form-urlencoded",
-        "x-rapidapi-host":"astrology-horoscope.p.rapidapi.com",
-        "x-rapidapi-key":"5caeaef20cmsh9c842b055ba0f73p144b7cjsncb86644bad8e",
-       
-        },
-        data:{
+      async: true,
+      crossDomain: true,
+      url: "https://astrology-horoscope.p.rapidapi.com/zodiac_compatibility/result",
+      method: "POST",
+      headers: {
+        "x-rapidapi-host": "astrology-horoscope.p.rapidapi.com",
+        "x-rapidapi-key": "5caeaef20cmsh9c842b055ba0f73p144b7cjsncb86644bad8e",
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      data: {
+        v4: "json",
         mystic_dob: yourDOB,
-        mystic_dob2:theirDOB,
-        mystic_name:yourName,
-        mystic_name2:theirName
-        }
-     }
-     $.ajax((settings, ( function(response) {
-            $("#contents").empty();
+        mystic_dob2: theirDOB,
+        mystic_name: yourName,
+        mystic_name2: theirName,
+      },
+    };
+    $.ajax(settings).done(function (response) {
+      $("#contents").empty();
 
-            $("#results").empty();
-            console.log(response.data)
-            var compatibility = response.data.data.Compatibility
-          
-        $("#results").append(card(compatibility));
-          })
-          ));
-    
-    
-        function card(compatibility) {
-    var createCard = ' <div class="card results-card"><div class="card-body"><h5 class="card-title">' + yourName +' <p>and</p> '+ theirName + ' <p>compatibility</p> ' + '</h5><p class="card-text">' + compatibility + '</p>';
-            return createCard;
-        } 
+      $("#results").empty();
+      console.log(response)
+
+      $("#results").append(card(response));
+    });
+  }
+//   var compatibility = data.data.Compatibility;
+  function card(response) {
+      var response = response;
+    var createCard =
+    ' <div style ="width: 90%;"  class="card results-card"><div class="card-body"><h3 class="card-title">Compatibility Results</h3><p class="card-text">' + response + '</p>';
+    return createCard;
+  }
+
+  $("#get-results").on("keypress click", function (event) {
+    event.preventDefault();
+    var yourName = $(".your-name").val().trim();
+    var theirName = $(".their-name").val().trim();
+    var yourDOB = $(".yourDOB").val().trim();
+    var theirDOB = $(".theirDOB").val().trim();
+   
+      compatibility(yourName, theirName, yourDOB, theirDOB);
     }
-
-
-    $("#get-results").on("keypress click", function (event) {
-        event.preventDefault();
-        var yourName = $(".your-name").val().trim()
-        var theirName = $(".their-name").val().trim()
-        var yourDOB = $(".yourDOB").val().trim()
-        var theirDOB = $(".theirDOB").val().trim()
-        
-            compatibility(yourName, theirName, yourDOB, theirDOB);
-    })
-
+  );
 });
