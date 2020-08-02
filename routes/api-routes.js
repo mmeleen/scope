c78660061bda970a81484edaa6d633ac726359cf
Matt Meleen <mmeleen@utexas.edu>
2020-08-02 14:03:58 -0500

    updated user_data route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,18 +4,18 @@ var passport = require('../config/passport');
 var astroJs = require('aztro-js');
 var axios = require('axios');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post('/api/login', passport.authenticate('local'), function(req, res) {
+  app.post('/api/login', passport.authenticate('local'), function (req, res) {
     res.json(req.user);
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post('/api/signup', function(req, res) {
+  app.post('/api/signup', function (req, res) {
     var signRes;
     axios({
       method: 'POST',
@@ -30,7 +30,7 @@ module.exports = function(app) {
         v4: 'json',
         mystic_dob: req.body.DOB,
       },
-    }).then(function(response) {
+    }).then(function (response) {
       // console.log(response.data);
       signRes = response.data.data.sunsign;
       db.User.create({
@@ -39,13 +39,13 @@ module.exports = function(app) {
         name: req.body.name,
         DOB: req.body.DOB,
         sign: signRes,
-      }).then(function() {
+      }).then(function () {
         res.redirect(307, '/api/login');
-      }).catch(function(err) {
+      }).catch(function (err) {
         // console.log(err);
         res.status(422).json(err.errors[0].message);
       });
-    }).catch(function() {
+    }).catch(function () {
       // if DOB is not valid
       res.status(422).json('Birthday is not valid');
     });
@@ -53,13 +53,13 @@ module.exports = function(app) {
   });
 
   // Route for logging user out
-  app.get('/logout', function(req, res) {
+  app.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
   });
 
   // Route for getting some data about our user to be used client side
-  app.get('/api/user_data', function(req, res) {
+  app.get('/api/user_data', function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
@@ -70,24 +70,26 @@ module.exports = function(app) {
         username: req.user.username,
         id: req.user.id,
         DOB: req.user.DOB,
+        name: req.user.name,
+        sign: req.user.sign
       });
     }
   });
 
   // Routes for getting horoscopes calling astroJs npm package
-  app.get('/api/allHoroscopes/', function(req, res) {
+  app.get('/api/allHoroscopes/', function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
     } else {
       var sign = req.user.sign;
-      astroJs.getAllHoroscope(sign, function(response) {
+      astroJs.getAllHoroscope(sign, function (response) {
         res.json(response);
       });
     }
   });
 
-  app.post('/api/saveSearch', function(req, res) {
+  app.post('/api/saveSearch', function (req, res) {
     db.Search.create({
       date: req.body.search.date,
       description: req.body.search.description,
@@ -97,34 +99,34 @@ module.exports = function(app) {
       lucky_time: req.body.search.lucky_time,
       UserId: req.user.id,
     })
-      .then(function(results){
+      .then(function (results) {
         res.json(results);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         // console.log(err);
         res.status(401).json(err);
       });
   });
 
   // return user object from database
-  app.post('/api/search/:username', function(req, res) {
-    db.sequelize.findOne({ where: { username: req.params.username } }, function(
+  app.post('/api/search/:username', function (req, res) {
+    db.sequelize.findOne({ where: { username: req.params.username } }, function (
       results
     ) {
       res.json(results);
     });
   });
 
-  app.get('/api/searches', function(req, res) {
+  app.get('/api/searches', function (req, res) {
     db.Search.findAll({
       where: {
         UserId: req.user.id,
       },
     })
-      .then(function(results) {
+      .then(function (results) {
         res.json(results);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         // console.log(err);
         res.status(401).json(err);
       });
